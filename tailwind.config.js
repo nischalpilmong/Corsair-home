d81655826e4c3d73057c3c1b2c24cf4648def089
nischalpilmong <nischalrai04@gmail.com>
2022-06-08 22:28:19 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ["./dist/**/*.{html,js}"],
+  theme: {
+    extend: {
+       height: {
+         '1/2-screen': '50vh'
+       }
+    },
+    fontFamily: {
+      'Nunito': ['Nunito', 'sans-serif'],
+      'Poppins': ['Poppins', 'sans-serif'],
+      'Caveat': ['Caveat', 'sans-serif'],
+    }
+  },
+  plugins: [],
+}
