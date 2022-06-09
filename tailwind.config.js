22ac975cdd28c2d1d34680d32fedea1cd2c98037
nischalpilmong <nischalrai04@gmail.com>
2022-06-09 22:02:33 +0200

    add sound-graph image and show the team member profile

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,15 +2,18 @@ module.exports = {
   content: ["./dist/**/*.{html,js}"],
   theme: {
     extend: {
+      colors: {
+        'yellow-green': 'linear-gradient(rgba(163, 251, 107, 0.6), rgba(163, 251, 107, 0.6) 0.01%, rgba(229, 251, 107, 0.6))'
+      },
        height: {
          '1/2-screen': '50vh'
+       },
+       fontFamily: {
+        'Nunito': ['Nunito', 'sans-serif'],
+        'Poppins': ['Poppins', 'sans-serif'],
+        'Caveat': ['Caveat', 'sans-serif'],
        }
-    },
-    fontFamily: {
-      'Nunito': ['Nunito', 'sans-serif'],
-      'Poppins': ['Poppins', 'sans-serif'],
-      'Caveat': ['Caveat', 'sans-serif'],
-    }
+     },
   },
   plugins: [],
 }
